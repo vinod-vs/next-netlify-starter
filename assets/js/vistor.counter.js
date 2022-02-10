a0f0c10880291d274e7475a9ce60246ac5c098a2
Siragaon Vinod <vsiragaon@woolworths.com.au>
2022-02-10 14:30:12 +1100

    Publishing the function

--- assets/js/vistor.counter.js
+++ assets/js/vistor.counter.js
@@ -2,11 +2,12 @@
 var counterContainer = document.querySelector(".website-counter");
 
 const functionAPI = 'http://localhost:7071/api/HttpTrigger1';
+const functionApiUrl = 'https://cloudresumechallenge2.azurewebsites.net/api/HttpTrigger1?code=Pf511QPdhaFyExIY7M0QU/Pi5MHD9yWhQmy3oWbpFhaakG9kQTtdZQ==';
 
 const getVisitCount = () => {
     var count;
     
-    fetch(functionAPI).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log(" Website called the function API");
